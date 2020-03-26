eb5dd09326ae74b394f675d48d508545a03ea06e
Tran Dung Chien <trandungchien1982@gmail.com>
2020-03-26 23:10:12 +0700

    03. Docker Image, Docker Container, Docker Registry 04. Demo Gradle+SpringBoot_staging

---
+++ 04. Gradle+SpringBoot_staging/sample-gradle-project/src/main/java/hello/Application.java
@@ -0,0 +1,28 @@
+package hello;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("\nDONE in Start Spring Boot ...");
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            System.out.println("Total beans : " + beanNames.length);
+        };
+    }
+
+}
